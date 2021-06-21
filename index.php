55810958a28dc41b06636963731ce1df4c66c65f
xxiaoqie@111.com <xxiaoqie@111.com>
2021-06-21 10:24:19 +0800

    modified with html and datatable

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 <title>邮箱系统</title>
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <script src="js/jquery-2.1.1.js"></script>
-<style>
-</style>
-<script>
+<!--<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" />-->
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
+<script type="text/javascript">
         function del(id){
             if(confirm("您确定要删除吗？")){
                 //location.href="del.php?id="+id;
@@ -22,6 +22,23 @@
 					   });
             }
         }
+		function SortTable(obj) {
+			if(obj.innerHTML == "发件人") {
+				location.href='index.php?order=name';
+			} else if(obj.innerHTML == "时间") {
+				location.href='index.php?order=time';	
+			}
+			
+		}
+		$(function(){
+			var table= $("#myTable").DataTable({
+				searching: false,
+    			ordering:  false,
+				bLengthChange: false,
+				bFilter: false,
+				bSort: false,
+				bInfo: false});
+		});
         window.onload = function(){
             document.getElementById("delSelected").onclick = function(){
                 if(confirm("您确定要删除选中条目吗？")){
@@ -52,31 +69,43 @@
 <?php
 include_once("conn/conn.php");
 ?>
-<div class="mailcontainer" style="padding-left:15%;">
-	<div> <a class="btn btn-default btn-sm" href="javascript:void(0);" id="delSelected">删除选中</a></div>
+<!--左侧导航栏-->
+<nav style="position: absolute;height:100px;width:150px;display:block">121231231231231</nav>
+<div style="position: relative !important; margin-left:150px;" class="container-fluid">
+	<!--头工具条-->
+	<div style="padding:5px;"> <a class="btn btn-default btn-sm" href="javascript:void(0);" id="delSelected">删除选中</a></div>
+    <!--邮件信息-->
 	<form id="form" action="del.php" method="post">
-        <table class="table table-hover table-condensed">
-            <tr class="success">
-                <th width="1%" class=""><input type="checkbox" id="firstCb"></th>
+        <table id="myTable" class="table table-hover table-condensed">
+            <thead><tr class="success">
+                <th class=""><input type="checkbox" id="firstCb"></th>
                 <th><table width="100%"><tr>
-                	<td width="20%">发件人</td>
+                	<td onclick="SortTable(this)" class="as" width="20%">发件人</td>
                     <td width="60%">主题</td>
-                    <td width="20%">时间</td>
+                    <td onclick="SortTable(this)"  class="as" width="20%">时间</td>
                 </tr></table></th>
-                <th width="5%">操作</th>
-            </tr>
+                <th>操作</th>
+            </tr></thead>
+            <tbody>
             <?php
-				$sqlstr = "select * from receive order by id";
+				$sqlstr = "select * from receive order by ";
+				if(!isset($_GET['order'])) {
+					$sqlstr = $sqlstr."id";
+				} else {
+					if($_GET['order'] == 'id') $sqlstr = $sqlstr."id";
+					if($_GET['order'] == 'time') $sqlstr = $sqlstr."stime";
+					if($_GET['order'] == 'name') $sqlstr = $sqlstr."sender";
+				}
 				$result = mysqli_query($conn,$sqlstr);
 				while( $rows = mysqli_fetch_array($result) ) {
 			?>
 			<tr>
                 <td><input type="checkbox" name="chk[]" value="<?php echo $rows['id']; ?>"/> </td>
                 <td>
-                	<a style="text-decoration:none" href="readmail.php?id=<?php echo $rows['id'];?>"><table width="100%"><tr>
-                    	<td width="20%"><?php echo $rows['sender']; ?></td>
-                        <td width="60%"><?php echo $rows['title']; ?></td>
-                        <td width="20%"><?php echo $rows['stime']; ?></td>
+                	<a style="text-decoration:none;<?php if($rows['isread']) echo "color:#777";?>" href="readmail.php?id=<?php echo $rows['id'];?>"><table width="100%"><tr>
+                    	<td style="text-overflow:ellipsis;overflow:hidden;display:inline-block;" width="20%"><?php echo $rows['sender']; ?></td>
+                        <td style="text-overflow:ellipsis;overflow:hidden;display:inline-block;" width="60%"><?php echo $rows['title']; ?></td>
+                        <td style="text-overflow:ellipsis;overflow:hidden;display:inline-block;" width="20%"><?php echo $rows['stime']; ?></td>
                     </tr></table></a>
                 </td>
 				<td><a href="#" onclick="del(<?php echo $rows['id'];?>)">删除</a></td>
@@ -84,8 +113,11 @@ include_once("conn/conn.php");
             <?php 
 				}
 			?>
+            </tbody>
     </table>
     </form>
+	<!--尾工具条-->
+	<div style="padding:5px;"> <a class="btn btn-default btn-sm" href="javascript:void(0);" id="delSelected">删除选中</a></div>
 </div>
 </body>
 </html>
--- readmail.php
+++ readmail.php
@@ -3,22 +3,30 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
 <?php include_once("conn/conn.php"); ?>
 <?php 
 $sqlstr = "select * from receive where id = ".$_GET['id'];
 $result = mysqli_query($conn,$sqlstr);
-$row = mysqli_fetch_array($result)
+$row = mysqli_fetch_array($result);
+$sqlstr = "update receive set isread = 1 where id = ".$_GET['id'];
+mysqli_query($conn,$sqlstr);
 ?>
 <title><?php echo $row['title'] ?></title>
 </head>
 
 <body>
-<div class="container-fluid">
-	<div style=" background-color:#C1D9F3; padding:5px" class="form-horizontal">
+<!--左侧导航栏-->
+<nav style="position: absolute;height:100px;width:150px">12123123</nav>
+<div style="position: relative !important; margin-left:150px;" class="container-fluid">
+	<!--头工具条-->
+	<div class="text-left" style=" background-color:#C1D9F3; padding:5px">
     	<a class="btn btn-default btn-sm" href="del.php?id=<?php echo $row['id']; ?>" >删除</a>
     </div>
-    <div style="background-color:#eff5fb; padding:10px">
-    	<h4><strong><?php echo $row['title'] ?></strong></h4>
+    
+    <!--信件信息-->
+    <div class="mail-box-header" style="background-color:#eff5fb; padding:10px">
+    	<h2><strong><?php echo $row['title'] ?></strong></h2>
         <table>
         	<tr>
             	<td>发件人</td>
@@ -30,10 +38,13 @@ $row = mysqli_fetch_array($result)
             </tr>
         </table>
     </div>
-    <div style="padding:20px 10px" class="">
-    	<?php echo $row['content']; ?>
+    
+    <div class="mail-box">
+    	<div class="mail-body"><?php echo $row['content']; ?></div>
     </div>
-	<div style=" background-color:#C1D9F3; padding:5px" >
+    
+	<!--尾工具条-->
+	<div class="text-left" style=" background-color:#C1D9F3; padding:5px" >
     	<a class="btn btn-default btn-sm" href="del.php?id=<?php echo $row['id']; ?>" >删除</a>
     </div>
 </div>
